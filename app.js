890364f8c8b27a3c1e4e91c3b3953d1ba23f0a99
Abrar ul Hassan <107148226+Abrarulhassan-786@users.noreply.github.com>
2023-04-07 00:10:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+// var rightnow = new Date();
+// var theDay = rightnow.getDate().toString();
+// var month = rightnow.getDate();
+// var year = rightnow.getFullYear();
+// var mint = rightnow.getMinutes();
+// console.log(theDay)
+// console.log(month)
+// console.log(year)
+// console.log(mint)
+
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// var getdate = new Date("june 30, 2024, 12:33:30");
+// var getDay = getdate.getDay();
+// var gettinginfor = days[getDay];
+// console.log(gettinginfor)
+// console.log(getdate)
+// var getDate = getdate.getDate();
+// var getdayhours = getdate.toString();
+// var useclice = getdayhours.slice(0,24);
+// console.log(getDate)
+// console.log(useclice)
+
+// How to calculate Your total days from birth to current
+// var today = new Date("Feb 14, 2002");
+// var dmoonsdy  = new Date();
+// var mstoday = today.getTime();
+// var msdmoosdy = dmoonsdy.getTime();
+// var msDiff = msdmoosdy-mstoday;
+// var ddiff  =msDiff/(1000*60*60*24);
+// console.log(today);
+// console.log(mstoday)
+// console.log(dmoonsdy);
+// console.log(msdmoosdy);
+// console.log("Different Milli Second : "+msDiff)
+// console.log("Days Differ : "+ddiff)
+
+// var d = new Date();
+// console.log(d)
+// var dchange = d.setFullYear(2024)
+// console.log(dchange)
+
+//passes data in function vallues
+// function calcTo(merchTot){
+//     var OrderTo;
+//     if(merchTot >= 100){
+//         OrderTo  = merchTot;
+//     }
+//     else if(merchTot  < 50){
+//         OrderTo = merchTot;
+//     }
+//     else{
+//         OrderTo = merchTot+6+(0.3*(merchTot-50))
+//     }
+//     return OrderTo;
+// }
+// var TotalToChanges = calcTo(79)
+// console.log(TotalToChanges);
+
+//Switch Statement
+// switch("Moday"){
+//     case "Tuesday":
+//         alert("Foun Tuesday")
+//         break;
+//     case "Webdnesday":
+//         alert("Found Wednesday")
+//         break;
+//     case "Thurday":
+//         alert("Found Thurdsday")
+//         break;
+//     default:
+//         alert("Try Again")
+// }
